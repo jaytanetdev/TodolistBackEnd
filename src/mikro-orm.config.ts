96119ed98582c659c26b0b585737e6bbf16c3874
jaytanetdev <jaytanetdev@gmail.com>
2025-03-23 20:27:25 +0700

    feat:pgadmin

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import { Options } from '@mikro-orm/core';
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { ConfigService } from '@nestjs/config';
 const configService = new ConfigService();
+
 const MikroOrmConfig: Options = {
   entities: ['./dist/**/*.entity.js'],
   entitiesTs: ['./src/**/*.entity.ts'],
@@ -10,11 +11,13 @@ const MikroOrmConfig: Options = {
     path: 'dist/migrations',
     pathTs: 'src/migrations',
   },
-  dbName: configService.get('POSTGRES_DB') || 'todolist',
-  user: configService.get('POSTGRES_USER') || 'root',
-  password: configService.get('POSTGRES_PASSWORD') || '123123Jay',
-  host: configService.get('POSTGRES_HOST') || 'localhost',
-  port: configService.get('POSTGRES_PORT') || 5432,
+  clientUrl: 'postgres:ISCvcFqZrLuhsaWOLbOaDfMkYHzwGHOD@tramway.proxy.rlwy.net:17989/railway'
+  // dbName: configService.get('POSTGRES_DB') || 'railway',
+  // user: configService.get('POSTGRES_USER') || 'postgres',
+  // password: configService.get('POSTGRES_PASSWORD') || 'ISCvcFqZrLuhsaWOLbOaDfMkYHzwGHOD',
+  // host: configService.get('POSTGRES_HOST') || 'postgres.railway.internal',
+  // port: configService.get('POSTGRES_PORT') || 5432,
+
 };
 
 export default MikroOrmConfig;
